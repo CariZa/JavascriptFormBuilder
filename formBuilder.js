64e017815d468e0838b106dfa1a8824b4cee22e4
CariZa <liebenbc@yahoo.com>
2016-04-19 20:54:37 +0200

    Major refactoring. Code is easier to extend and build on. Seperated into more functions.

--- formBuilder.js
+++ formBuilder.js
@@ -14,138 +14,9 @@ var formBuilder = function(id) {
       'parent' : id
     };
 
-    var valid_types = [
-        'input',
-        'select',
-        'option'
-    ];
+    //Utils
 
-    //NB: this is doing too much, seperate - think Open/Closed principle / Liskov's
-    // Perhaps have a form class, which pulls in form element classes
-    // Form -> Input, Select, Textare, Checkbox, Radio etc.
-
-    this.dom = (function () {
-
-      var api = {};
-
-      api.clear = function(id) {
-        var element = api.getElementById(id);
-        element.innerHTML = '';
-      };
-
-      api.createInput = function(data) {
-        return document.createElement('input');
-      };
-
-      api.wrapFormElements = function(list) {
-        var elements = [];
-        for(var i = 0; i < list.length; i++){
-            var div = document.createElement('div');
-            div.setAttribute("class", settings.divClass);
-            div.appendChild(list[i]);
-            elements.push(div);
-        }
-        return elements;
-      };
-
-      api.createSelect = function(data) {
-        var select = document.createElement('select');
-        if (data.options) {
-          var options = api.buildElements(data.options);
-          api.appendChildren(options, select);
-        }
-        return select;
-      };
-
-      api.createOption = function(data) {
-        return document.createElement('option');
-      };
-
-      api.createLabel = function(value) {
-        var label = document.createElement('label');
-        label.innerText = value;
-        return label;
-      }
-
-      api.fetchElement = function(data) {
-          var createTypes = {
-              'input' : api.createInput,
-              'select' : api.createSelect,
-              'option' : api.createOption
-          };
-
-          if (createTypes[data.type]) {
-              return createTypes[data.type](data);
-          }
-      };
-
-      api.getElementById = function(id) {
-          return document.getElementById(id);
-      };
-
-      api.appendChildren = function(list, addTo) {
-          for(var i = 0; i < list.length; i++){
-              addTo.appendChild(list[i]);
-          }
-      };
-
-      api.invalidType = function(type) {
-          return valid_types.indexOf(type) < 0;
-      };
-
-      api.buildElements = function(list) {
-          var elements = [];
-          for (var i = 0; i < list.length; i++) {
-              if (list[i].label) {
-                elements.push(api.createLabel(list[i].label));
-              }
-              elements.push(api.buildElement(list[i]));
-          }
-          return elements;
-      };
-
-      api.buildAttributes = function(element, list) {
-        for (var j = 0; j < Object.keys(list).length; j++) {
-            var attribute = Object.keys(list)[j];
-            var value = list[attribute];
-            element.setAttribute(attribute, value);
-        }
-        return element;
-      };
-
-      api.buildElement = function(data, type) {
-          //Validation hack, refactor when have time
-
-          if (api.invalidType(data.type)) return;
-
-          var element = api.fetchElement(data);
-          if (data.attributes) {
-              element = api.buildAttributes(element, data.attributes);
-          }
-          if (data.text) {
-              element.innerHTML = data.text;
-          }
-          return element;
-
-      };
-
-      api.create = function(list, id) {
-            //var container = DOMManipulator().getElementById(id);
-            var formElements = dom.buildElements(list);
-            dom.clear(id);
-            var element = dom.getElementById(id)
-            var elements = dom.wrapFormElements(formElements);
-            dom.appendChildren(elements, element);
-      };
-
-      return api;
-
-    })();
-
-
-
-
-    this.configureSelectOptions = (function() {
+    var configureSelectOptions = function() {
 
       var api = {};
 
@@ -185,89 +56,253 @@ var formBuilder = function(id) {
 
       return api;
 
-    })();
+    };
 
-    var Add = (function() {
+    //
+
+    var Radio = function() {
+        var api = {};
+
+        api.add = function(formFields, list, data) {
+
+        };
+
+        api.create = function(data) {
+
+        };
+
+        return {
+            add : function(formFields, list, data) {
+                api.add(formFields, list, data)
+            },
+            create: function(data) {
+                return api.create(data);
+            }
+        };
+    };
+
+    var Select = function() {
 
         var api = {};
-        var configOpt = this.configureSelectOptions;
 
-        api.addInput = function(data) {
-          var data = data || {};
-          form_fields.push({
-              type : 'input',
-              attributes : {
-                  name: data.name || '',
-                  value: data.value || '',
-                  class: data.class || settings.inputClass,
-                  placeholder : data.placeholder || ''
-              },
-              label: data.label || false
+        var configOpt = configureSelectOptions();
+
+        api.add = function (formFields, data) {
+            var data = data || {};
+            form_fields.push({
+                  type: 'select',
+                  attributes : {
+                    name : data.name,
+                    selected : data.selected, //NB: TODO add this feature
+                    class: data.class || settings.selectClass
+                  },
+                  label: data.label || false,
+                  options : configOpt.createOptions(data.options)
             });
         };
 
-        api.addSelect = function(list, data) {
-          var data = data || {};
-          form_fields.push({
-              type: 'select',
-              attributes : {
-                name : data.name,
-                selected : data.selected, //NB: TODO add this feature
-                class: data.class || settings.selectClass
-              },
-              label: data.label || false,
-              options : configOpt.createOptions(list)
-          });
-          debugger;
+        api.createOption = function(list) {
+            return document.createElement('option');
+        }
+
+        api.create = function(data) {
+            var select = document.createElement('select');
+            if (data.options) {
+              for(var i = 0; i < data.options.length; i++) {
+                  var option = api.createOption(data.options);
+                  option.innerText = data.options[i].text
+                  select.appendChild(option);
+              }
+            }
+            return select;
         };
+
+        return {
+            add : function(formFields, list, data) {
+                api.add(formFields, list, data)
+            },
+            create: function(data) {
+                return api.create(data);
+            }
+        };
+
+    };
+
+    var Input = function() {
+
+        var api = {};
+
+        api.add = function (formFields, data) {
+            var data = data || {};
+            formFields.push({
+                type : 'input',
+                attributes : {
+                    name: data.name || '',
+                    value: data.value || '',
+                    class: data.class || settings.inputClass,
+                    placeholder : data.placeholder || ''
+                },
+                label: data.label || false
+            });
+        }
+
+        api.create = function() {
+            return document.createElement('input');
+        }
+
+        return {
+            add : function(formFields, data) {
+                api.add(formFields, data);
+            },
+            create : function() {
+                return api.create();
+            }
+        };
+
+    };
+
+
+    var formUtils = function() {
+        var api = {};
+        api.clear = function(id) {
+            var element = document.getElementById(id);
+            element.innerHTML = '';
+        };
+        api.wrapFormElements = function(list) {
+            var elements = [];
+            for(var i = 0; i < list.length; i++){
+              var div = document.createElement('div');
+              div.setAttribute("class", settings.divClass);
+              div.appendChild(list[i]);
+              elements.push(div);
+            }
+            return elements;
+        };
+        api.appendChildren = function(list, addTo) {
+            for(var i = 0; i < list.length; i++){
+                addTo.appendChild(list[i]);
+            }
+        };
+        api.createLabel = function(value) {
+            var label = document.createElement('label');
+            label.innerText = value;
+            return label;
+        };
+        return {
+            clear: function(id) {
+                api.clear(id);
+            },
+            wrapFormElements : function(list) {
+                return api.wrapFormElements(list);
+            },
+            appendChildren: function(list, addTo) {
+                api.appendChildren(list, addTo);
+            },
+            createLabel : function(value) {
+                return api.createLabel(value);
+            }
+        };
+    };
+
+    var Build = function(form_fields, form, utils) {
+
+        var formUtils = utils;
+        var api = {};
+        var form_fields = form_fields;
+        var form = form;
+
+        var types = {
+            'input' : "Input",
+            'select' : "Select"
+        };
+
+        api.buildAttributes = function(element, list) {
+            for (var j = 0; j < Object.keys(list).length; j++) {
+              var attribute = Object.keys(list)[j];
+              var value = list[attribute];
+              element.setAttribute(attribute, value);
+            }
+            return element;
+        };
+
+        api.buildElement = function(data, type) {
+            var formType = types[type];
+            var element = form[formType].create(data);
+            if (data.attributes) {
+                element = api.buildAttributes(element, data.attributes);
+            }
+            if (data.text) {
+                element.innerHTML = data.text;
+            }
+            return element;
+        };
+
+        api.buildElements = function(list) {
+            var elements = [];
+            for (var i = 0; i < list.length; i++) {
+                if (list[i].label) {
+                  elements.push(utils.createLabel(list[i].label));
+                }
+                elements.push(api.buildElement(list[i], list[i].type));
+            }
+            return elements;
+        }
+
+        api.init = function(list, id){
+            var formElements = api.buildElements(list);
+            utils.clear(id);
+            var element = document.getElementById(id)
+            var elements = utils.wrapFormElements(formElements);
+            utils.appendChildren(elements, element);
+        };
+
+        api.addFormType = function(type, data) {
+            form[types[type]].add(form_fields, data);
+        };
+
+        return {
+            build : function(id) {
+                api.init(form_fields, id)
+            },
+            add : function(type, data) {
+                api.addFormType(type, data);
+                form_fields;
+                this.build("form_wrapper");
+            }
+        };
+
+    };
+
+    //Purpose of form is to give access to elements
+    var form = function() {
+
+        var parent = this;
+        var api = {
+            addFeature : function(key, feature) {
+                addFeature(key, feature())
+            }
+        };
+
+        function addFeature(key, feature) {
+            api[key] = feature;
+        }
 
         return api;
 
-    })();
+    };
 
-
-
-    var Controls = function (dom) {
-
-      var dom = dom;
-
-      return {
-                init : function() {
-                  dom.create(form_fields, settings.parent);
-                  return instance;
-                },
-                addSettings : function(customSettings) {
-                  try {
-                    var keys = Object.keys(customSettings);
-                    for (var i = 0; i < keys.length; i++) {
-                      settings[keys[i]] = customSettings[keys[i]];
-                    }
-                  }
-                  catch(error) {
-
-                  }
-                  return this.init();
-                },
-                addInput : function(options) {
-                  Add.addInput(options);
-                  return this.init();
-                },
-                addSelect : function(list, data) {
-                  Add.addSelect(list, data);
-                  return this.init();
-                }
-                // addParent : function(id) {
-                //   settings.parent = id;
-                //   return instance;
-                // }
-          };
-
-    }
+    var utilsInstace = new formUtils();
+    var formInstance = new form();
+    formInstance.addFeature("Input", Input);
+    formInstance.addFeature("Select", Select);
+    formInstance.addFeature("Radio", Radio);
 
     //Singleton
     function init() {
       if (!instance) {
-        instance = new Controls(this.dom);
+        var buildInstance = new Build(form_fields, formInstance, utilsInstace);
+        buildInstance.build("form_wrapper");
+        instance = buildInstance;
       }
       return instance;
     }
@@ -276,56 +311,4 @@ var formBuilder = function(id) {
 
 };
 
-
-
-
-
-function addInput(form, inputId) {
-
-  var data = {
-    name: '',
-    class: '',
-    label: document.getElementById(inputId).value
-  };
-
-  form.addInput(data);
-  document.getElementById(inputId).value = '';
-
-  var text = document.createTextNode(document.getElementById('form_wrapper').innerHTML);
-  var el = document.getElementById('displayHTML');
-  el.appendChild(text);
-
-}
-
-function addSelection(list, valueInputId, textInputId) {
-
-  var selection = document.getElementById(valueInputId).value;
-
-  list.push(selection);
-
-  var listContainer = document.getElementById('displayList');
-  listContainer.innerHTML = '';
-
-  for (var i = 0; i < list.length; i++) {
-    var li = document.createElement('li');
-    li.setAttribute('class','list-group-item');
-    li.innerHTML = list[i];
-    listContainer.appendChild(li);
-  }
-
-  return list;
-
-}
-
-function addSelect(form, list, labelInputId) {
-
-  var data = {
-    name: '',
-    value: '',
-    class: '',
-    label: document.getElementById(labelInputId).value
-  };
-
-  form.addSelect(list, data);
-
-}
+var form = formBuilder("form_wrapper");
